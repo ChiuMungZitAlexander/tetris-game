8d5d6f1cd5e20e89448626f5fb4edb951e19a542
AlexanderZhao <zhaomengzhe@qq.com>
2018-10-15 18:34:24 +0800

    add tetris comp

---
+++ src/components/tetris/tetris.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+/* 
+  Tetris: 20 rows, 10 cols
+*/
+
+const Tetris: React.SFC = () => (
+  <div className="tetris-container">
+    tetris
+  </div>
+)
+
+export default Tetris
--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react'
 
 import ControlPanel from '../../components/controlPanel/controlPanel'
+import Tetris from '../../components/tetris/tetris'
 
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
+        <div className="play-area">
+          <section className="info-section">info</section>
+          <section className="tetris-section">
+            <Tetris />
+          </section>
+        </div>
         <ControlPanel test="test" />
       </div>
     )
