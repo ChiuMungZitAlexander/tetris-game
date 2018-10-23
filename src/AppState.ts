5f3d15326cae05bcab5a0b398b9fa629593913e7
AlexanderZhao <zhaomengzhe@qq.com>
2018-10-23 17:26:44 +0800

    add auto fall and stack

--- src/AppState.ts
+++ src/AppState.ts
@@ -1,22 +1,36 @@
 import { action, observable } from 'mobx'
 
-import { generateData } from './utils'
+import { generateData, generateTetris } from './utils'
 
 export default class AppState {
   @observable public inProgress: boolean = false
   @observable public data: boolean[][] = generateData()
+  @observable public currentCor: number[] = [0, 4]
+  @observable public currentTetris: boolean[][] = generateTetris()
 
-  @action public fall = () => {
-    const randomNum = [Math.floor(Math.random() * 20), Math.floor(Math.random() * 10)]
-    this.data = this.data.map((row, r) => (
-      r === randomNum[0]
-        ? row.map((col, c) => (
-          c === randomNum[1]
-            ? true
-            : col
-        ))
-        : row
-    ))
+  @action public fall = (): void => {
+    const newData = this.data.map(row => row.slice()) // deep clone
     
+    if (this.currentCor[0] + 1 > 19) {
+      newData[this.currentCor[0]][this.currentCor[1]] = true
+      if (this.currentCor[0] > 0) {
+        newData[this.currentCor[0] - 1][this.currentCor[1]] = false
+      }
+      this.currentCor = [0, 4]
+    } else if (this.data[this.currentCor[0] + 1][this.currentCor[1]]) {
+      newData[this.currentCor[0]][this.currentCor[1]] = true
+      if (this.currentCor[0] > 0) {
+        newData[this.currentCor[0] - 1][this.currentCor[1]] = false
+      }
+      this.currentCor = [0, 4]
+    } else {
+      newData[this.currentCor[0]][this.currentCor[1]] = true
+      if (this.currentCor[0] > 0) {
+        newData[this.currentCor[0] - 1][this.currentCor[1]] = false
+      }
+      this.currentCor = this.currentCor.map((cor, i) => i ? cor : cor + 1)
+    }
+
+    this.data = newData
   }
 }
